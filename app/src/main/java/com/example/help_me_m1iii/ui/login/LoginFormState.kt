b43bba8452aa16a56da81f8c82d7b8c114109d4d
Unknow46 <nathan.sens@hotmail.fr>
2020-02-20 14:26:44 +0100

    Initial commit

---
+++ app/src/main/java/com/example/help_me_m1iii/ui/login/LoginFormState.kt
@@ -0,0 +1,8 @@
+package com.example.help_me_m1iii.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState (val usernameError: Int? = null,
+                      val passwordError: Int? = null,
+                      val isDataValid: Boolean = false)
---
+++ app/src/main/java/com/example/help_me_m1iii/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.example.help_me_m1iii.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult (
+     val success:LoggedInUserView? = null,
+     val error:Int? = null
+)
