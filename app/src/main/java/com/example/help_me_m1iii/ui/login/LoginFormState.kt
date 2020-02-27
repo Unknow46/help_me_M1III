30e41a0876c742126f14ecb60be25020a4934682
Unknow46 <nathan.sens@hotmail.fr>
2020-02-27 10:14:22 +0100

    Adding firebase to project

--- app/src/main/java/com/example/help_me_m1iii/ui/login/LoginFormState.kt
+++ app/src/main/java/com/example/help_me_m1iii/ui/login/LoginFormState.kt
@@ -3,6 +3,8 @@ package com.example.help_me_m1iii.ui.login
 /**
  * Data validation state of the login form.
  */
-data class LoginFormState (val usernameError: Int? = null,
-                      val passwordError: Int? = null,
-                      val isDataValid: Boolean = false)
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
--- app/src/main/java/com/example/help_me_m1iii/ui/login/LoginResult.kt
+++ app/src/main/java/com/example/help_me_m1iii/ui/login/LoginResult.kt
@@ -3,7 +3,7 @@ package com.example.help_me_m1iii.ui.login
 /**
  * Authentication result : success (user details) or error message.
  */
-data class LoginResult (
-     val success:LoggedInUserView? = null,
-     val error:Int? = null
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
 )
